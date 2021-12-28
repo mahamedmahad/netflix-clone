c891171d7ab27c0391a5967302a4e8cdd7a9590d
Mahamed Mahad <mahameddeeq26@hotmail.com>
2021-12-28 01:07:59 +0100

    refactored feature container

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,16 @@
 import React from 'react';
-import {OptForm} from '../components/';
-import {Feature} from '../components';
+
 import {HeaderContainer} from "../containers/header";
 import {JumbotronContainer} from '../containers/jumbotron';
 import {FooterContainer} from "../containers/footer";
 import {FaqsContainer} from "../containers/faqs";
+import {FeatureContainer} from '../containers/feature';
 
 export default function Home() {
     return (
         <>
             <HeaderContainer>
-                <Feature>
-                    <Feature.Title>
-                        Unlimited films, tv and more.
-                    </Feature.Title>
-                    <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
-                    <OptForm>
-                        <OptForm.Input placeholder="Email address"/>
-                        <OptForm.Button>Try it now</OptForm.Button>
-                        <OptForm.BreakLine/>
-                        <OptForm.Text>
-                            Ready to Watch? Enter your email to create or restart membership
-                        </OptForm.Text>
-                    </OptForm>
-                </Feature>
+                <FeatureContainer />
             </HeaderContainer>
             <JumbotronContainer/>
             <FaqsContainer/>
